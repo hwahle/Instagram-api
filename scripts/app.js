f57ef9d0757c161aae7b372087cf95c6dc564633
hwahle <helenwahle@gmail.com>
2016-02-03 10:44:54 -0800

    templating

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 var feed = new Instafeed({
     get: 'user',
     userId: '641301337',
-    accessToken: '641301337.c4d52ef.6dc4d6719ce645d0a8826ab507a419d9'
+    accessToken: '641301337.c4d52ef.6dc4d6719ce645d0a8826ab507a419d9',
+    template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
 })
 
 feed.run()

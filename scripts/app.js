f0d1a4d3e733e0d700e6653ca5b7267409f7ea6c
hwahle <helenwahle@gmail.com>
2016-02-15 19:40:43 -0800

    integrated lightbox

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
     get: 'user',
     userId: '641301337',
     accessToken: '641301337.c4d52ef.6dc4d6719ce645d0a8826ab507a419d9',
-    template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
+    template: '<a href="#" data-featherlight="{{image}}"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
 })
 
 feed.run()

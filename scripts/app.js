6bb668fc0809bf09f088aba3f891e9c2f2eaea66
hwahle <helenwahle@gmail.com>
2016-02-17 11:01:33 -0800

    fixed resolution issue in templating options

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 var feed = new Instafeed({
     get: 'user',
     userId: '641301337',
+    resolution: 'standard_resolution',
     accessToken: '641301337.c4d52ef.6dc4d6719ce645d0a8826ab507a419d9',
-    template: '<a href="#" data-featherlight="{{image}}"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
+    template: '<a href="#" data-featherlight="{{image}}"><img src="{{image}}" width="150" height="150" /><div class="likes">&hearts; {{likes}}</div></a>'
 })
 
 
